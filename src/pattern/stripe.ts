f7602ad1ae9652a661103da42e165d7862e4f09e
ChallenAi <fengyiai@gmail.com>
2025-01-22 11:23:42 +0000

    feat: add a stripe pattern

---
+++ src/pattern/stripe.ts
@@ -0,0 +1,73 @@
+/**
+ * Stripe Propertities
+ */
+export interface StripeProps {
+  /**
+   * width of the stripe
+   */
+  width: number;
+  /**
+   * height of the stripe
+   */
+  height: number;
+  /**
+   * line width of the stripe
+   */
+  lineWidth: number;
+  /**
+   * margin between lines of the stripe
+   */
+  margin: number;
+  /**
+   * offset controls the stripe slope
+   */
+  offset: number;
+  /**
+   * line color
+   */
+  lineColor?: string;
+  /**
+   * background color
+   */
+  backgroundColor?: string;
+  /**
+   * segements for dotted line, for example: [12, 3]
+   */
+  segements?: number[];
+};
+
+/**
+ * draw a stripe
+ */
+export const drawStripe = (
+  ctx: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D,
+  {
+    width,
+    height,
+    lineWidth,
+    margin,
+    offset,
+    lineColor,
+    backgroundColor,
+    segements,
+  }: StripeProps,
+) => {
+  if (backgroundColor) ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, width, height);
+
+  const unitWidth = lineWidth + margin;
+  const num = (width + Math.abs(offset)) / unitWidth + 2;
+  for (let i = 0; i < num; i++) {
+    if (offset < 0) {
+      ctx.moveTo(i * unitWidth + offset, 0);
+      ctx.lineTo(i * unitWidth, height);
+    } else {
+      ctx.moveTo(i * unitWidth, 0);
+      ctx.lineTo(i * unitWidth - offset, height);
+    }
+    ctx.lineWidth = lineWidth;
+    if (lineColor) ctx.strokeStyle = lineColor;
+    if (segements) ctx.setLineDash(segements);
+    ctx.stroke();
+  }
+};
