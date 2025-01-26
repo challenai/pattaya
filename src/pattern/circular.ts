c855be734b23012ca12e4f26b4eb741f14591948
ChallenAi <fengyiai@gmail.com>
2025-01-26 11:46:07 +0000

    feat: improve performance of draw circular pattern function

--- src/pattern/circular.ts
+++ src/pattern/circular.ts
@@ -59,21 +59,36 @@ export const drawCircular = (
 ) => {
   ctx.save();
 
-  // const p = new Path2D();
-  // p.rect(0, 0, width, height);
-  // ctx.clip(p);
-  if (backgroundColor) ctx.fillStyle = backgroundColor;
-  ctx.fillRect(0, 0, width, height);
+  const p = new Path2D();
+  p.rect(0, 0, width, height);
+  ctx.clip(p);
+
+  if (backgroundColor) {
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height);
+  }
 
   ctx.translate(offsetX, offsetY);
+  ctx.lineWidth = lineWidth;
+  if (lineColor) ctx.strokeStyle = lineColor;
+  if (segements) ctx.setLineDash(segements);
 
-  for (let i = 0; i < 100; i++) {
-    ctx.arc(0, 0, margin * i, 0, Math.PI, true);
-    ctx.lineWidth = lineWidth;
-    if (lineColor) ctx.strokeStyle = lineColor;
-    if (segements) ctx.setLineDash(segements);
-    ctx.stroke();
+  const x1 = Math.abs(offsetX);
+  const y1 = Math.abs(offsetY);
+  const x2 = Math.abs(offsetX - width);
+  const y2 = Math.abs(offsetY - height);
+  const minR = Math.min(x1, y1, x2, y2);
+  const mx = Math.max(x1, x2);
+  const my = Math.max(y1, y2);
+  const maxR = Math.sqrt(mx * mx + my * my) + margin;
+  let r = minR - minR % margin;
+  ctx.beginPath();
+  while (r <= maxR) {
+    ctx.arc(0, 0, r, 0, 2 * Math.PI);
+    r += margin;
+    ctx.moveTo(r, 0);
   }
+  ctx.stroke();
 
   ctx.restore();
 };
