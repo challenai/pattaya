a15bb6b2a59f74dcae43a480819df04dd86a9e27
ChallenAi <fengyiai@gmail.com>
2025-01-27 02:37:11 +0000

    feat: colorland improve popup docs

--- colorland/content/docs/popup.tsx
+++ colorland/content/docs/popup.tsx
@@ -1,36 +1,36 @@
 'use client'
 import { ShadowElement } from "@pattaya/depict/graph";
-import { popup } from "@pattaya/pattaya";
+import { popup, theme } from "@pattaya/pattaya";
 
-const muted = { border: "#666", background: "#ccf", shadow: "#fff", shadowBlur: 0 };
+export const background = theme.mayk.popup.popupBackground;
+
+const popupStyle = theme.mayk.popup.popupStyle;
 
 export const basic: ShadowElement[][] = [
   [
     {
       x: 180,
       y: 120,
-      shapes: popup.blueprint.shape({ width: 120, height: 75, radius: 9, triangleWidth: 18, triangleHeight: 10 }, muted),
+      shapes: popup.blueprint.shapes({ width: 120, height: 75, radius: 9, triangleWidth: 18, triangleHeight: 10 }, popupStyle.normal),
     },
   ],
 ];
-
-const active = { border: "#666", background: "#aae", shadow: "#fff", shadowBlur: 0 };
 
 export const custom: ShadowElement[][] = [
   [
     {
       x: 180,
       y: 120,
-      shapes: popup.blueprint.shape({ width: 120, height: 75, radius: 9, triangleWidth: 18, triangleHeight: 10 }, muted),
+      shapes: popup.blueprint.shapes({ width: 120, height: 75, radius: 9, triangleWidth: 18, triangleHeight: 10 }, popupStyle.normal),
       contain(x, y) {
         return x > 0 && y > 0 && x < 120 && y < 90;
       },
       onMouseenter(render, x, y, mouseX, mouseY) {
-        popup.blueprint.applyStyle(this.shapes, active);
+        popup.blueprint.applyStyle(this.shapes, popupStyle.active);
         render();
       },
       onMouseleave(render, x, y, mouseX, mouseY) {
-        popup.blueprint.applyStyle(this.shapes, muted);
+        popup.blueprint.applyStyle(this.shapes, popupStyle.normal);
         render();
       },
     },
