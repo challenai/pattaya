a4f7a503b759e7f858a0c442ae7fcb8b7ca44cf5
ChallenAi <fengyiai@gmail.com>
2025-01-28 16:07:09 +0000

    chore: colorland merge pattern mdx and code files

---
+++ colorland/content/docs/pattern.ts
@@ -0,0 +1,72 @@
+'use client'
+import { ShadowElement } from "@pattaya/depict/graph";
+import { stripe as s, circular as c } from "@pattaya/pattaya";
+
+export const stripe: ShadowElement[][] = [
+  [
+    {
+      x: 120,
+      y: 90,
+      postRenderCallback(ctx) {
+        s.drawStripe(ctx, {
+          width: 100,
+          height: 100,
+          lineWidth: 1,
+          margin: 9,
+          offset: -100,
+          segements: [10, 6],
+          lineColor: "#ccf",
+        });
+      }
+    },
+  ],
+];
+
+export const circular: ShadowElement[][] = [
+  [
+    {
+      x: 120,
+      y: 90,
+      postRenderCallback(ctx) {
+        c.drawCircular(ctx, {
+          width: 100,
+          height: 100,
+          lineWidth: 1,
+          margin: 9,
+          offsetX: -20,
+          offsetY: -20,
+          lineColor: "#ccf",
+        });
+      }
+    },
+  ],
+];
+
+export const grid: ShadowElement[][] = [
+  [
+    {
+      x: 120,
+      y: 90,
+      postRenderCallback(ctx) {
+        c.drawCircular(ctx, {
+          width: 100,
+          height: 100,
+          lineWidth: 1,
+          margin: 9,
+          offsetX: -20,
+          offsetY: -20,
+          lineColor: "#ccf",
+        });
+        // g.drawGrid(ctx, {
+        //   width: 100,
+        //   height: 100,
+        //   lineWidth: 1,
+        //   margin: 9,
+        //   offsetX: -20,
+        //   offsetY: -20,
+        //   lineColor: "#ccf",
+        // });
+      }
+    },
+  ],
+];
