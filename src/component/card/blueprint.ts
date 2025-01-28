44879ac229e95de8be7df57408b36a7885ffbdc0
ChallenAi <fengyiai@gmail.com>
2025-01-28 16:02:15 +0000

    chore: format card code

--- src/component/card/blueprint.ts
+++ src/component/card/blueprint.ts
@@ -1,6 +1,6 @@
 import type { Mesh, MeshOptions } from "@pattaya/depict/graph";
 import type { CardStyleProps } from "./props";
-import { Rectangle } from "@pattaya/pather";
+import { rectangle } from "impressionist";
 
 export interface CardProps {
   width: number,
@@ -8,29 +8,31 @@ export interface CardProps {
   radius: number,
 };
 
-const setOpts = (style: CardStyleProps): MeshOptions => ({
-  fill: style.background,
-  stroke: style.border,
-  shadowColor: style.shadow,
-  shadowBlur: style.shadowBlur,
-});
+function setOpts(style: CardStyleProps): MeshOptions {
+  return {
+    fill: style.background,
+    stroke: style.border,
+    shadowColor: style.shadow,
+    shadowBlur: style.shadowBlur,
+  };
+}
 
-export const applyStyle = (shape: Mesh[] | undefined, style: CardStyleProps) => {
+export function applyStyle(shape: Mesh[] | undefined, style: CardStyleProps) {
   shape![0].opts = setOpts(style);
-};
+}
 
-export const shapes = ({ width, height, radius }: CardProps, style: CardStyleProps): Mesh[] => {
+export function shapes({ width, height, radius }: CardProps, style: CardStyleProps): Mesh[] {
   return [
     {
-      path: Rectangle.RoundAligned(0, 0, width, height, radius),
+      path: rectangle.roundAligned(0, 0, width, height, radius),
       opts: setOpts(style),
     },
   ];
-};
+}
 
-export const wireframe = ({ width, height, radius }: CardProps): string => {
-  return Rectangle.RoundAligned(0, 0, width, height, radius)
-};
+export function wireframe({ width, height, radius }: CardProps): string {
+  return rectangle.roundAligned(0, 0, width, height, radius);
+}
 
 export default {
   shapes,
