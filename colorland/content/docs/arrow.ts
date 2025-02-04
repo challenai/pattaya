c5c1b47da10363c5e7c482879131cba1f5001e05
ChallenAi <fengyiai@gmail.com>
2025-02-04 16:22:23 +0000

    feat: colorland improve arrows docs

---
+++ colorland/content/docs/arrow.ts
@@ -0,0 +1,112 @@
+'use client'
+import { Graph } from "@pattaya/depict/graph";
+import { arrow, theme } from "@pattaya/pattaya";
+
+export const background = theme.mayk.arrow.arrowBackground;
+const styles = theme.mayk.arrow.arrowStyle;
+
+export const bGraph = new Graph();
+bGraph.onReady(() => {
+  bGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.basic.shapes({ width: 16, height: 20 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.basic.shapes({ width: 16, height: 20 }, styles.active),
+    },
+  ]]);
+});
+
+export const tGraph = new Graph();
+tGraph.onReady(() => {
+  tGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.triangle.shapes({ width: 16, height: 20 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.triangle.shapes({ width: 16, height: 20 }, styles.active),
+    },
+  ]]);
+});
+
+export const vGraph = new Graph();
+vGraph.onReady(() => {
+  vGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.vee.shapes({ width: 16, low: -6, high: 12 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.vee.shapes({ width: 16, low: -6, high: 12 }, styles.active),
+    },
+    {
+      x: 180,
+      y: 240,
+      shapes: arrow.vee.shapes({ width: 12, low: 6, high: 18 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 240,
+      shapes: arrow.vee.shapes({ width: 12, low: 6, high: 18 }, styles.active),
+    },
+  ]]);
+});
+
+export const buGraph = new Graph();
+buGraph.onReady(() => {
+  buGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.bullet.shapes({ width: 12, height: 18 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.bullet.shapes({ width: 12, height: 18 }, styles.active),
+    },
+  ]]);
+});
+
+export const blGraph = new Graph();
+blGraph.onReady(() => {
+  blGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.blunt.shapes({ width: 16, low: 12, high: 24 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.blunt.shapes({ width: 16, low: 12, high: 24 }, styles.active),
+    },
+  ]]);
+});
+
+export const dGraph = new Graph();
+dGraph.onReady(() => {
+  dGraph.resetGraph([[
+    {
+      x: 180,
+      y: 120,
+      shapes: arrow.dome.shapes({ width: 16, low: 12 }, styles.normal),
+    },
+    {
+      x: 300,
+      y: 120,
+      shapes: arrow.dome.shapes({ width: 16, low: 12 }, styles.active),
+    },
+  ]]);
+});
