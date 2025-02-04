03948488c2cf75ff214b0661061570db16214938
ChallenAi <fengyiai@gmail.com>
2025-02-04 08:53:03 +0000

    refactor: add radius support to step line

--- src/primitives/line/step.ts
+++ src/primitives/line/step.ts
@@ -2,6 +2,11 @@ import type { Mesh, MeshOptions } from "@pattaya/depict/graph";
 import type { LineStyles } from "./styles";
 import { step } from "impressionist";
 
+export interface StepLineProps {
+  points: number[];
+  radius?: number;
+};
+
 function toOpts(style: LineStyles): MeshOptions {
   return {
     stroke: style.color,
@@ -12,17 +17,17 @@ export function applyStyle(shape: Mesh[] | undefined, style: LineStyles) {
   shape![0].opts = toOpts(style);
 };
 
-export function shapes(points: number[], style: LineStyles): Mesh[] {
+export function shapes(props: StepLineProps, style: LineStyles): Mesh[] {
   return [
     {
-      path: wireframe(points),
+      path: wireframe(props),
       opts: toOpts(style),
     },
   ];
 };
 
-export function wireframe(pathes: number[]): string {
-  return step.basic(0, 0, pathes);
+export function wireframe({ points, radius }: StepLineProps): string {
+  return step.relative(0, 0, points, radius);
 };
 
 export default {
