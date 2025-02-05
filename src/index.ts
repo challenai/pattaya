cbb934f75bb65237b03f96bb9489c8fb08dfd90b
ChallenAi <fengyiai@gmail.com>
2025-02-05 04:21:11 +0000

    chore: expose components and animation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * as primitives from "./primitives";
-// export * as components from "./components";
+export * as components from "./components";
 export * as patterns from "./patterns";
 export * as themes from "./themes";
 export * as mayk from "./themes/mayk";
+export * as animation from "./animation";
