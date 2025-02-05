3b47c6a88ebb571eba652c264ae05f3853f01f24
ChallenAi <fengyiai@gmail.com>
2025-02-05 14:19:04 +0000

    feat: add get endpoints direction methods of quadratic bezier curve

--- src/primitives/line/quadratic.ts
+++ src/primitives/line/quadratic.ts
@@ -1,7 +1,8 @@
 import type { Mesh, MeshOptions } from "@pattaya/depict/graph";
 import type { LineStyles } from "./styles";
-import type { Point } from "./point";
+import type { Endpoint, Point } from "./point";
 import { bezier } from "impressionist";
+import { points2Radians } from "../../utils/ratotation";
 
 function toOpts(styles: LineStyles): MeshOptions {
   const opts: MeshOptions = {};
@@ -32,9 +33,39 @@ export function wireframe(points: Point[]): string {
   return bezier.quadratic(points);
 };
 
+export function start(points: Point[]): Endpoint | null {
+  if (!points || points.length < 3) return null;
+  const m = points[0];
+  const n = points[1];
+  return {
+    x: m.x,
+    y: m.y,
+    rotation: points2Radians(m.x, m.y, n.x, n.y),
+  };
+};
+
+export function end(points: Point[]): Endpoint | null {
+  if (!points || points.length < 3) return null;
+  const n = points[points.length - 1];
+  let x = points[1].x;
+  let y = points[1].y;
+  for (let i = 2; i < points.length - 1; i++) {
+    const p = points[i];
+    x = 2 * p.x - x;
+    y = 2 * p.y - y;
+  }
+  return {
+    x: n.x,
+    y: n.y,
+    rotation: points2Radians(x, y, n.x, n.y),
+  };
+};
+
 export default {
   shapes,
   wireframe,
   toOpts,
   applyStyle,
+  start,
+  end,
 };
