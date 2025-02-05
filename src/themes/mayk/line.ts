ab51af82ebc55f2405df55493dd25bdbd09c9e1f
ChallenAi <fengyiai@gmail.com>
2025-02-05 05:11:02 +0000

    feat: add line and pattern styles

---
+++ src/themes/mayk/line.ts
@@ -0,0 +1,13 @@
+export { default as background } from "./background";
+import palette from "./palette";
+
+export const styles = {
+  normal: {
+    color: palette.baby,
+    width: 1,
+  },
+  active: {
+    color: palette.navy,
+    width: 1,
+  },
+};
