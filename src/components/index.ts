7afdafee1b96481b1be302d2ad9f6d587f8e97e6
ChallenAi <fengyiai@gmail.com>
2025-02-09 13:03:48 +0000

    feat: adjust exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
 export * as arrow from "./arrow";
 export * as circular from "./circular";
+export * as edge from "./edge";
+export * as line from "./line";
 export * as nodes from "./nodes";
 export * as popup from "./popup";
-export * as split from "./split";
-export * as line from "./line";
-export * as button from "./button";
-export * as card from "./card";
 export * as slider from "./slider";
+export * as split from "./split";
