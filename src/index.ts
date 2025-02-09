f53fbd3e1b61223ce88065f7f6114f04cf68ace2
ChallenAi <fengyiai@gmail.com>
2025-02-09 13:46:35 +0000

    refactor: remove specific themes from top level exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * as core from "./core";
 export * as components from "./components";
 export * as patterns from "./patterns";
 export * as themes from "./themes";
-export * as mayk from "./themes/mayk";
