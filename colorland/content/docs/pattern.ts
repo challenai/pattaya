d94d1c1a0ef2c8e508f157acdfea32305a3500e3
ChallenAi <fengyiai@gmail.com>
2025-02-09 04:34:46 +0000

    feat: colorland improve pattern doc

--- colorland/content/docs/pattern.ts
+++ colorland/content/docs/pattern.ts
@@ -1,72 +1,59 @@
 'use client'
-import { ShadowElement } from "@pattaya/depict/graph";
-import { stripe as s, ring as r } from "@pattaya/pattaya";
+import { Graph } from "@pattaya/depict/graph";
+import { stripe, circular, grid } from "@pattaya/pattaya/patterns";
 
-export const stripe: ShadowElement[][] = [
-  [
-    {
-      x: 120,
-      y: 90,
-      postRenderCallback(ctx) {
-        s.drawStripe(ctx, {
-          width: 100,
-          height: 100,
-          lineWidth: 1,
-          margin: 9,
-          offset: -100,
-          segements: [10, 6],
-          lineColor: "#ccf",
-        });
-      }
-    },
-  ],
-];
+const sn = {
+  x: 120,
+  y: 90,
+  postRenderCallback(ctx: any) {
+    stripe.drawStripe(ctx, {
+      width: 100,
+      height: 100,
+      lineWidth: 1,
+      margin: 9,
+      offset: -100,
+      segements: [10, 6],
+      lineColor: "#ccf",
+    });
+  }
+};
 
-export const ring: ShadowElement[][] = [
-  [
-    {
-      x: 120,
-      y: 90,
-      postRenderCallback(ctx) {
-        r.drawRing(ctx, {
-          width: 100,
-          height: 100,
-          lineWidth: 1,
-          margin: 9,
-          offsetX: -20,
-          offsetY: -20,
-          lineColor: "#ccf",
-        });
-      }
-    },
-  ],
-];
+const rn = {
+  x: 120,
+  y: 90,
+  postRenderCallback(ctx: any) {
+    circular.drawCircular(ctx, {
+      width: 100,
+      height: 100,
+      lineWidth: 1,
+      margin: 9,
+      offsetX: -20,
+      offsetY: -20,
+      lineColor: "#ccf",
+    });
+  }
+};
 
-export const grid: ShadowElement[][] = [
-  [
-    {
-      x: 120,
-      y: 90,
-      postRenderCallback(ctx) {
-        r.drawRing(ctx, {
-          width: 100,
-          height: 100,
-          lineWidth: 1,
-          margin: 9,
-          offsetX: -20,
-          offsetY: -20,
-          lineColor: "#ccf",
-        });
-        // g.drawGrid(ctx, {
-        //   width: 100,
-        //   height: 100,
-        //   lineWidth: 1,
-        //   margin: 9,
-        //   offsetX: -20,
-        //   offsetY: -20,
-        //   lineColor: "#ccf",
-        // });
-      }
-    },
-  ],
-];
+const gn = {
+  x: 120,
+  y: 90,
+  postRenderCallback(ctx: any) {
+    grid.drawGrid(ctx, {
+      width: 100,
+      height: 100,
+      lineWidth: 1,
+      marginX: 9,
+      marginY: 9,
+      offsetX: 3.5,
+      offsetY: 5.5,
+      lineColor: "#ccf",
+    });
+  }
+};
+
+export const sGraph = new Graph();
+sGraph.onReady(() => sGraph.resetGraph([[sn]]));
+export const rGraph = new Graph();
+rGraph.onReady(() => rGraph.resetGraph([[rn]]));
+export const gGraph = new Graph();
+gGraph.onReady(() => gGraph.resetGraph([[gn]]));
