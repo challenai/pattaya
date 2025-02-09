6050ef183d53ac4ffc8c8a5bf7a6d9593237d120
ChallenAi <fengyiai@gmail.com>
2025-02-09 13:50:08 +0000

    refactor: add a button example

--- colorland/content/docs/button.ts
+++ colorland/content/docs/button.ts
@@ -1,7 +1,74 @@
 'use client'
 import { Graph } from "@pattaya/depict/graph";
-import { button } from "@pattaya/pattaya/components";
+import type { ShadowElement, TextOptions } from "@pattaya/depict/graph";
+import { nodes } from "@pattaya/pattaya/components";
+import { animationRunning, newAnimationStore, startAnimation, updateAnimation, rectContain } from "@pattaya/pattaya/core";
 
+export interface TextStyles {
+  textColor: string;
+}
+
+export interface TextButtonStyles extends nodes.NodeStyles, TextStyles { }
+
+export interface TextButtonProps {
+  radius?: number;
+  styles: {
+    normal: TextButtonStyles;
+    active: TextButtonStyles;
+    common: {
+      font?: string;
+      textLineHeight?: number;
+    };
+  };
+  text: string;
+  textBoundingBox: any;
+  onClick?: () => void;
+}
+
+export function TextButton(x: number, y: number, { radius, styles, text, textBoundingBox, onClick }: TextButtonProps): ShadowElement {
+  if (!radius) radius = 0;
+  const textOpts: TextOptions = {
+    textAlign: "center",
+    textBaseline: "middle",
+    fill: styles.normal.textColor,
+  };
+  if (styles.common.font && styles.common.textLineHeight) {
+    textOpts.font = styles.common.font;
+    textOpts.lineHeight = styles.common.textLineHeight;
+  }
+  const buttonText = {
+    content: text,
+    opts: textOpts,
+  };
+  const bb = textBoundingBox(buttonText);
+  const width = bb.width + 36;
+  const height = bb.height + 24;
+  const contain = rectContain(width, height, false);
+  return {
+    x,
+    y,
+    shapes: nodes.rectangle.shapes({ width, height, radius }, styles.normal),
+    contain,
+    texts: [buttonText],
+    data: { animation: newAnimationStore(styles.normal) },
+    update(ts: number) {
+      if (animationRunning(this.data.animation)) {
+        const result = updateAnimation<TextButtonStyles>(this.data.animation, ts);
+        nodes.rectangle.applyStyle(this.shapes, result);
+        this.texts![0].opts!.fill = result.textColor;
+      }
+    },
+    onMouseenter() {
+      startAnimation(this.data.animation, 250, styles.active);
+    },
+    onMouseleave() {
+      startAnimation(this.data.animation, 250, styles.normal);
+    },
+    onClick,
+  };
+}
+
+// -----------------------------------------------
 export const graph = new Graph();
 
 export const background = "#fff";
@@ -26,11 +93,7 @@ const styles = {
 };
 
 graph.onReady(() => {
-  const btn = button.basic.TextButton(240, 180, { text: "click me", radius: 8, styles, textBoundingBox: graph.boundingBox.bind(graph) });
+  const btn = TextButton(240, 180, { text: "click me", radius: 8, styles, textBoundingBox: graph.boundingBox.bind(graph) });
   graph.updateLayerOptions(0, { dynamic: true, update: true });
-  graph.resetGraph([[{
-    x: 0,
-    y: 0,
-    children: [btn]
-  }]]);
+  graph.resetGraph([[btn]]);
 });
