a3fc8f742817794b7e51463be88427400b053ffb
ChallenAi <fengyiai@gmail.com>
2025-02-10 16:44:31 +0000

    refactor: improve rotation, don't export radians > Pi/2, use negative radians

--- src/core/ratotation.ts
+++ src/core/ratotation.ts
@@ -5,11 +5,8 @@ export function points2Degrees(x0: number, y0: number, x1: number, y1: number): 
 export function points2Radians(x0: number, y0: number, x1: number, y1: number): number {
   const dx = x1 - x0;
   const dy = y1 - y0;
-  if (dy === 0) {
-    return x0 <= x1 ? 0 : Math.PI;
-  }
   if (dx === 0) {
-    return y0 <= y1 ? Math.PI * 0.5 : Math.PI * 1.5;
+    return y0 <= y1 ? Math.PI / 2 : - Math.PI / 2;
   }
   const r = Math.atan(dy / dx);
   if (dx < 0) return r + Math.PI;
