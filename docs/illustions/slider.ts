547a342663d58f2355d054ca67306108ac757d35
ChallenAi <fengyiai@gmail.com>
2025-02-19 16:21:15 +0000

    docs: improve slider example

---
+++ docs/illustions/slider.ts
@@ -0,0 +1,34 @@
+import { Graph } from "@pattaya/depict/graph";
+import theme from "../theme";
+import { slider } from "@pattaya/pattaya/components";
+
+const ident = "ptySlider";
+
+const sliderProps = { totalLength: 200, progress: .62, barWidth: 3, slideWidth: 3, radius: 6 };
+const n = {
+  x: 240,
+  y: 130,
+  shapes: slider.basic.shapes(sliderProps, theme.ptr.slider.normal),
+  contain(x: number, y: number) {
+    return x > 0 && x < sliderProps.totalLength && y > -10 && y < 10;
+  },
+  onClick(render: any, _x: number, _y: number, mx: number, _my: number) {
+    sliderProps.progress = (mx - this.x) / sliderProps.totalLength;
+    slider.basic.update(this.shapes, sliderProps);
+    render();
+  }
+};
+
+const graph = new Graph();
+graph.onReady(() => graph.resetGraph([[n]]));
+
+function applyTheme() {
+  slider.basic.applyStyle(n.shapes, theme.ptr.slider.normal);
+  graph.renderAll();
+}
+
+export default {
+  ident,
+  graph,
+  applyTheme,
+}
